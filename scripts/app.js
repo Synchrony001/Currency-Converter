3534ef34f3108b8a0a429a6a8f4dad62fb52edc7
@Synchrony001 <achemaku@gmail.com>
2018-07-10 10:34:17 +0100

    Updated repo

--- scripts/app.js
+++ scripts/app.js
@@ -15,8 +15,8 @@ function getCurrencies() {
  
       for(var key in values){
       const currencyId = key;
-      const currencyItem = values[key].currencyName;      
-      options += `<option value="${currencyId}">${currencyItem}</option>`;                 
+      const currencyName = values[key].currencyName;
+      options += `<option value="${currencyId}">${currencyName}</option>`;                 
     } 
  
   document.getElementById("fromcur").innerHTML = options;
@@ -38,41 +38,32 @@ function saveToIdb(data){
 	  tx,
 	  store,
 	  index;
-
 	  request.onupgradeneeded = function(e) {
 		let db = request.result,
 			store = db.createObjectStore("curconstore", {
 			  keyPath: "query"})          
 	  };
-
 	  request.onerror = function(e) {
-		console.log("Booo:" + e.target.errorCode);
+		console.log("Error: " + e.target.errorCode);
 	  }
-
 	  request.onsuccess = function(e) {
 		db = request.result;
 		tx = db.transaction("curconstore", "readwrite");
 		store = tx.objectStore("curconstore");
 
 		db.onerror = function(e) {
-		  console.log("Booo:" + e.target.errorCode);
+		  console.log("Error: " + e.target.errorCode);
 		}
-
 		let putData = {"query" : query, "ratio":data[query]};
-
 		store.add(putData);
-
-		console.log(putData);  
-		
 	  }
-	console.log("The data is: " + data[query]);
 
 }
 
 
 
 document.getElementById("tocur").addEventListener('change', (e) => {
-	
+	document.getElementById("resultamount").setAttribute('value',"");
 	const fromcur = document.getElementById("fromcur").value;
 	const tocur = document.getElementById("tocur").value;
 	const amount = document.getElementById("amount").value;
@@ -83,7 +74,6 @@ document.getElementById("tocur").addEventListener('change', (e) => {
 		query = `${fromcur}_${tocur}`;
 		const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`;
 		
-		//console.log(url);
 		fetch(url).then(function(response) {
 		return response.json();
 		}).then(function(data) {
@@ -91,26 +81,23 @@ document.getElementById("tocur").addEventListener('change', (e) => {
 		const ratio = data[query];
 		document.getElementById("resultamount").setAttribute('value',(amount * ratio).toFixed(2));
 		}).catch(function(){
-			console.log("You are offline")     
+			console.log("Application offline")     
 			let req = indexedDB.open("curcon", 1);
 			req.onsuccess = function (e) {
 			let db = e.target.result;
 			let tx = db.transaction("curconstore", "readwrite");
 			let store = tx.objectStore("curconstore");
 			let dbData = store.get(query);
-			dbData.onerror = function () {
-			document.getElementById("resultamount").setAttribute('value',"Offline conversion failed!");
+			dbData.onerror = function (e) {
+				document.getElementById("amount").setAttribute('value',"");
+				document.getElementById("resultamount").setAttribute('value',"There is no Internet connection");
 			}
 
-			dbData.onsuccess = function () {
+			dbData.onsuccess = function (e) {
 			let dbRatio = dbData.result.ratio;
-			document.getElementById("resultamount").setAttribute('value',"Offline Results : " + (amount * dbRatio).toFixed(2));
+			document.getElementById("resultamount").setAttribute('value',(amount * dbRatio).toFixed(2));
 			}
 		  }    
 	  })  
 	}	
 });
-
-
-
-
--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,37 @@
-var cacheName = 'curcon-stattic-v2';
+var cacheName = 'curcon-stattic-v3';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/styles/inline.css',
-  'https://free.currencyconverterapi.com/api/v5/currencies',
-  '/favicon.ico'
+  './',
+   './manifest.json',
+  './index.html',
+  './scripts/app.js',
+  './styles/inline.css',
+  './favicon.ico',
+  'https://free.currencyconverterapi.com/api/v5/currencies'
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
+self.addEventListener('install', e=>{
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(caches.open(cacheName).then(cache=>{
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+    }
+    ), );
+}
+);
+
+
+self.addEventListener('activate', e=>{
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(caches.keys().then(keyList=>Promise.all(keyList.map(key=>{
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
+            return caches.delete(key);
         }
-      }));
-    })
-  );});
+    }
+    ), ), ), );
+}
+);
 
-/**
- * Serve app from cache if there is a cached version
- */
 self.addEventListener('fetch', function(event){    
     console.log('[Service Worker] Fetch', event.request.url);
     var requestUrl = new URL(event.request.url);
@@ -52,3 +50,4 @@ self.addEventListener('fetch', function(event){
         })        
       );
  });
+
